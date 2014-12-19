41fd5b064a3c404d98d687e44c21b9880aafa7b5
Prashant Dwivedi <dwi.pra24@gmail.com>
2014-12-19 14:06:27 +0530

    Initial Commit

---
+++ Restaurant ListTests/Restaurant_ListTests.m
@@ -0,0 +1,34 @@
+//
+//  Restaurant_ListTests.m
+//  Restaurant ListTests
+//
+//  Created by Prashant Dwivedi on 19/12/14.
+//  Copyright (c) 2014 Prashant Dwivedi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Restaurant_ListTests : XCTestCase
+
+@end
+
+@implementation Restaurant_ListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
